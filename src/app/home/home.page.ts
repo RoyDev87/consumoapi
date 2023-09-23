6ff0318f60a29f6efc7de7a45dd9f872000ef90d
roydev87 <gustavo.a.pezzini.p@gmail.com>
2023-09-22 22:16:15 -0300

    consumiendo api pokemon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  constructor( private pkService:PokemonService) {}
+
+  ngOnInit():void{
+    this.pkService.getCharacters().subscribe(
+      (data) => {
+        console.log(data);
+      }
+    );
+  }
 
 }
---
+++ src/app/pokemon.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getCharacters():Observable<any> {
+    return this.http.get('https://pokeapi.co/api/v2/pokemon?limit=151');
+  }
+}
